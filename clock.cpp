96720944088275a08cad9141bf29647e5fd32416
Kevin Jang <tul56945@temple.edu>
2025-02-14 18:19:54 -0500

    importing headers

--- clock.cpp
+++ clock.cpp
@@ -1,4 +1,7 @@
 //This project is over an engineering interview request. The parameters of this project is to design a timer/clock that does NOT utilize any clock or time libraries/headers. 
+#include <stdio.h>
+
+using namespace std;
 
 int main () {
     return 0;
