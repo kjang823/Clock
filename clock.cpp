dde6bc7ba585be0957d4eb05b6d10cc22b9eed57
Kevin Jang <tul56945@temple.edu>
2025-02-14 18:21:46 -0500

    Including sysinfoapi header

--- clock.cpp
+++ clock.cpp
@@ -1,5 +1,6 @@
 //This project is over an engineering interview request. The parameters of this project is to design a timer/clock that does NOT utilize any clock or time libraries/headers. 
 #include <stdio.h>
+#include <sysinfoapi.h>
 
 using namespace std;
 
