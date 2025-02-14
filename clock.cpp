3ed2480cef9efc31d76dc0aee2c1de7c81b1744d
Kevin Jang <tul56945@temple.edu>
2025-02-13 21:23:51 -0500

    init

---
+++ clock.cpp
@@ -0,0 +1,5 @@
+//This project is over an engineering interview request. The parameters of this project is to design a timer/clock that does NOT utilize any clock or time libraries/headers. 
+
+int main () {
+    return 0;
+}
